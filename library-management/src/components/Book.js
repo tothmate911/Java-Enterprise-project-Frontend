dcd6c10690b29662f94edebd725ddf52292aaf1f
Tóth Máté <tothmate911@gmail.com>
2020-06-25 13:34:37 +0200

    set default image of book

--- library-management/src/components/Book.js
+++ library-management/src/components/Book.js
@@ -2,29 +2,20 @@ import React from 'react';
 import noCover from './no-cover.webp';
 
 const Book = (props) => {
-  let content = (
+  let { authors, title, image } = props.book;
+
+  if (!image) {
+    image = noCover;
+  }
+
+  return (
     <div className="card bg-light">
-      <img className="card-img-top" src={noCover} alt=""></img>
+      <img className="card-img-top" src={image} alt={title}></img>
       <div className="card-body">
-        <h5 className="card-title">Book title</h5>
+        <h8 className="card-title">{`${authors}: ${title}`}</h8>
       </div>
     </div>
   );
-
-  if (props.book) {
-    const { authors, title, image } = props.book;
-
-    content = (
-      <div className="card bg-light">
-        <img className="card-img-top" src={image} alt={title}></img>
-        <div className="card-body">
-          <h8 className="card-title">{`${authors}: ${title}`}</h8>
-        </div>
-      </div>
-    );
-  }
-
-  return content;
 };
 
 export default Book;
