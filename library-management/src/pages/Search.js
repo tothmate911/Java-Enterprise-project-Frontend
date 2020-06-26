7599a41156b3ad2f86b35aeed35547057e088410
noemi <gynoemi33@gmail.com>
2020-06-26 04:11:21 +0200

    Search by author, and title

--- library-management/src/pages/Search.js
+++ library-management/src/pages/Search.js
@@ -1,51 +1,57 @@
 import React, { useContext, useEffect, useState } from "react";
-import { BookContext } from "../context/BookContext";
 import { Link } from "react-router-dom";
+import { StyleContext } from "../context/StyleContext";
+import axios from "axios";
 
 function Search() {
-  const [fetchedData, isLoading] = useContext(BookContext);
   const [search, setSearch] = useState("");
-  const [foundBooks, setFoundBooks] = useState([]);
+  const [result, setResult] = useState("");
+  const [Button] = useContext(StyleContext);
+  const [searchBy, setSearchBy] = useState("title");
+  let resultList = <span></span>;
 
-  const handleChange = (event) => {
-    const fieldInput = event.target.value.toLowerCase();
-    const searchedTitle = fetchedData.filter((book) =>
-      book.title.toLowerCase().includes(fieldInput)
-    );
-    if (fieldInput !== "") {
-      setFoundBooks(searchedTitle);
-    } else {
-      setFoundBooks([]);
-    }
-    setSearch(fieldInput);
+  const handleClick = (searchType) => {
+    setSearchBy(searchType);
+    setSearch(document.getElementById("search-field").value.toLowerCase());
+    resultList = <span></span>;
   };
 
-  const searchResults = foundBooks.map((book) => (
-    <div key={book.isbn13}>
-      <Link to={`/book/${book.isbn13}`}>
-        <li>
-          {book.authors}: {book.title}
-        </li>
-      </Link>
-    </div>
-  ));
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/books/search/" + searchBy + "/" + search, {})
+      .then((response) => setResult(response.data));
+  }, [search]);
 
-  let resultList = <span></span>;
-  if (!isLoading) {
-    resultList = searchResults;
+  if (result) {
+    resultList = result.map((book) => (
+      <div key={book.isbn13}>
+        <Link to={`/book/${book.isbn13}`}>
+          <li>
+            {book.authors}: {book.title}
+          </li>
+        </Link>
+      </div>
+    ));
   }
 
   return (
     <React.Fragment>
       <h1>Search</h1>
-      <input
-        type="text"
-        class="form-control mt-5"
-        placeholder="Search for book"
-        value={search}
-        onChange={handleChange}
-      />
-      {resultList}
+      <form>
+        <input
+          type="text"
+          id="search-field"
+          class="form-control mt-5"
+          placeholder="Search for book"
+        />
+        <Button type="button" onClick={() => handleClick("Title")}>
+          by Title
+        </Button>
+        <Button type="button" onClick={() => handleClick("Authors")}>
+          by Author
+        </Button>
+      </form>
+      <span id="result-list">{resultList}</span>
     </React.Fragment>
   );
 }
