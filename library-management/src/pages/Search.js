7138bdae9c7448f7e66f225d800fbb2285d174f4
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-06-26 08:10:04 +0200

    search-field converted to real time search

--- library-management/src/pages/Search.js
+++ library-management/src/pages/Search.js
@@ -1,59 +1,80 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { StyleContext } from "../context/StyleContext";
+import React, {useContext, useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import {StyleContext} from "../context/StyleContext";
 import axios from "axios";
 
 function Search() {
-  const [search, setSearch] = useState("");
-  const [result, setResult] = useState("");
-  const [Button] = useContext(StyleContext);
-  const [searchBy, setSearchBy] = useState("title");
-  let resultList = <span></span>;
+    //const [search, setSearch] = useState("");
+    const [result, setResult] = useState("");
+    //const [Button] = useContext(StyleContext);
+    //const [searchBy, setSearchBy] = useState("title");
+    const [filteredBooks, setFilteredBooks] = useState([]);
+    // let resultList = <span></span>;
 
-  const handleClick = (searchType) => {
-    setSearchBy(searchType);
-    setSearch(document.getElementById("search-field").value.toLowerCase());
-    resultList = <span></span>;
-  };
+    /*    const handleClick = (searchType) => {
+            setSearchBy(searchType);
+            setSearch(document.getElementById("search-field").value.toLowerCase());
+            resultList = <span></span>;
+        };*/
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/books/search/" + searchBy + "/" + search, {})
-      .then((response) => setResult(response.data));
-  }, [searchBy, search]);
+    useEffect(() => {
+        axios
+            .get("http://localhost:8080/books/", {})
+            .then((response) => {
+                setResult(response.data)
+                //setFilteredBooks(response.data)
+            })
+    }, []);
 
-  if (result) {
-    resultList = result.map((book) => (
-      <div key={book.isbn13}>
-        <Link to={`/book/${book.isbn13}`}>
-          <li>
-            {book.authors}: {book.title}
-          </li>
-        </Link>
-      </div>
-    ));
-  }
 
-  return (
-    <React.Fragment>
-      <h1>Search</h1>
-      <form>
-        <input
-          type="text"
-          id="search-field"
-          class="form-control mt-5"
-          placeholder="Search for book"
-        />
-        <Button type="button" onClick={() => handleClick("Title")}>
-          by Title
-        </Button>
-        <Button type="button" onClick={() => handleClick("Authors")}>
-          by Author
-        </Button>
-      </form>
-      <span id="result-list">{resultList}</span>
-    </React.Fragment>
-  );
+    const handleQuery = (event) => {
+        const fieldInput = event.target.value.toLowerCase()
+        const searchedBook = result.filter(item => item.title.toLowerCase().includes(fieldInput))
+        if (fieldInput !== '') {
+            setFilteredBooks(searchedBook)
+        } else if(fieldInput === ''){
+            setFilteredBooks([]);
+        }else{
+            setFilteredBooks(searchedBook)
+
+        }
+    }
+
+    const listBooks =
+        filteredBooks.map((item, index) => (
+            <div key={index}>
+                <li>{item.title}</li>
+            </div>
+        ));
+
+
+    /*if (result) {
+      resultList = result.map((book) => (
+        <div key={book.isbn13}>
+          <Link to={`/book/${book.isbn13}`}>
+            <li>
+              {book.authors}: {book.title}
+            </li>
+          </Link>
+        </div>
+      ));
+    }*/
+
+    return (
+        <React.Fragment>
+            <h1>Search</h1>
+            <form>
+                <input
+                    onChange={handleQuery}
+                    type="text"
+                    id="search-field"
+                    class="form-control mt-5"
+                    placeholder="Search for book"
+                />
+            </form>
+            <span id="result-list">{listBooks}</span>
+        </React.Fragment>
+    );
 }
 
 export default Search;
