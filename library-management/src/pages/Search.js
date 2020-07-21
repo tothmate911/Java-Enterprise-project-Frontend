892cd991d7d4792c85a33c8f35f29ef43a33992c
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-07-21 14:10:21 +0200

    refactored search: - fetches books from backend by realtime input

--- library-management/src/pages/Search.js
+++ library-management/src/pages/Search.js
@@ -1,45 +1,47 @@
-import React, { useContext, useState } from 'react';
-import { BookContext } from '../context/BookContext';
+import React, {useContext, useEffect, useState} from 'react';
+import {BookContext} from '../context/BookContext';
 import BookList from '../components/BookList';
+import axios from 'axios';
 
 function Search() {
-  const [allBooks, allBooksIsLoading] = useContext(BookContext);
-  const [filteredBooks, setFilteredBooks] = useState([]);
+    const [filteredBooks, setFilteredBooks] = useState([]);
 
-  let content = <h3>Loading...</h3>;
+    let content = <h3>Loading...</h3>;
 
-  if (!allBooksIsLoading && allBooks) {
+    const [input, setInput] = useState('@');
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/books/searchby/${input}`)
+            .then((response) => {
+                setFilteredBooks(response.data)
+            })
+
+    }, [input])
+
     const handleQuery = (event) => {
-      const fieldInput = event.target.value.toLowerCase();
-      const searchedBook = allBooks.filter((item) =>
-        item.title.toLowerCase().includes(fieldInput)
-      );
-      if (fieldInput !== '') {
-        setFilteredBooks(searchedBook);
-      } else if (fieldInput === '') {
-        setFilteredBooks([]);
-      } else {
-        setFilteredBooks(searchedBook);
-      }
-    };
+        const fieldInput = event.target.value.toLowerCase();
+        setInput(fieldInput);
+        if (fieldInput.length < 1)
+            setInput('@')
+    }
 
     content = (
-      <React.Fragment>
-        <h1>Search</h1>
-        <form>
-          <input
-            onChange={handleQuery}
-            type="text"
-            id="search-field"
-            className="form-control mt-5"
-            placeholder="Search for book"
-          />
-        </form>
-        <BookList books={filteredBooks} booksIsLoading={false} />
-      </React.Fragment>
+        <React.Fragment>
+            <h1>Search</h1>
+            <form>
+                <input
+                    onChange={handleQuery}
+                    type="text"
+                    id="search-field"
+                    className="form-control mt-5"
+                    placeholder="Search for book"
+                />
+            </form>
+            <BookList books={filteredBooks} booksIsLoading={false}/>
+        </React.Fragment>
     );
-  }
-  return content;
+
+    return content;
 }
 
 export default Search;
