e1f008d9a6954d6db83bd75365e934a0b75bd0cd
Tóth Máté <tothmate911@gmail.com>
2020-08-03 16:05:12 +0200

    add login page

---
+++ library-management/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <input type="text" id="uername" placeholder="username"></input>
+      <input type="password" id="password" placeholder="password"></input>
+      <button id="login">Login</button>
+    </div>
+  );
+};
+
+export default Login;
